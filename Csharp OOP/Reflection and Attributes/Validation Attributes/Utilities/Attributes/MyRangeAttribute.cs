c5f600741452e275cc20446ee72c8210e2a3c2dd
AnitaNakeva <anita_nakevaa@abv.bg>
2022-07-28 03:03:06 +0300

    added validation attributes

---
+++ Csharp OOP/Reflection and Attributes/Validation Attributes/Utilities/Attributes/MyRangeAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValidationAttributes.Utilities
+{
+    internal class MyRangeAttribute : MyValidationAttribute
+    {
+        private readonly int minValue;
+        private readonly int maxValue;
+
+        public MyRangeAttribute(int min, int max)
+        {
+            this.minValue = min;
+            this.maxValue = max;
+        }
+        public override bool IsValid(object obj)
+        {
+            if (obj is int val)
+            {
+                return val >= minValue && val <= maxValue;
+            }
+
+            return false;
+        }
+    }
+}
