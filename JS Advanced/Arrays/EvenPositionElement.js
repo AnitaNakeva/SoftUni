412d26a78e62d19b1b37526d6b7472056cbf421b
AnitaNakeva <anita_nakevaa@abv.bg>
2022-09-20 09:48:40 +0300

    added EvenPositionElement.js

---
+++ JS Advanced/Arrays/EvenPositionElement.js
@@ -0,0 +1,10 @@
+function evenPositionElement(arr){
+    let result = '';
+    for(let i = 0; i< arr.length; i+=2){
+        result+=arr[i];
+        result+=' ';
+    }
+    console.log(result);
+}
+
+evenPositionElement(['20', '30', '40', '50', '60']);
