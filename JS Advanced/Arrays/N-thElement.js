138ee390cd794f5b035b9ea610bc56a532f47c61
AnitaNakeva <anita_nakevaa@abv.bg>
2022-09-22 22:48:36 +0300

    added AddAndRemoveElements,js

--- JS Advanced/Arrays/N-thElement.js
+++ JS Advanced/Arrays/N-thElement.js
@@ -1,10 +1,8 @@
 function nthElement(arr, step){
     let result = [];
     for(let i = 0; i<arr.length; i+=step){
-        result.push(arr[i]);
+    result.push(arr[i]);
     }
-    
-    console.log(result.join(', '));
     return result;
 }
 
