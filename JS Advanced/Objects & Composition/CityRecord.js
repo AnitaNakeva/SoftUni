e6b8ccd21d38c480adbfef174d28cdc49aa7ae1c
AnitaNakeva <anita_nakevaa@abv.bg>
2022-09-24 16:42:35 +0300

    added CityRecord.js

---
+++ JS Advanced/Objects & Composition/CityRecord.js
@@ -0,0 +1,13 @@
+function cityRecord(name, population, treasury){
+    const city = {};
+    city['name']=name;
+    city['population'] = population;
+    city['treasury'] = treasury;
+
+    console.log(city);
+}
+
+cityRecord('Tortuga',
+7000,
+15000
+);
