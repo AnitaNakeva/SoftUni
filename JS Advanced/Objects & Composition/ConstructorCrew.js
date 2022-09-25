c40b25b37478051b1deed02936eb8ac609861ee0
AnitaNakeva <anita_nakevaa@abv.bg>
2022-09-25 18:16:38 +0300

    added ConstructorCrew.js

---
+++ JS Advanced/Objects & Composition/ConstructorCrew.js
@@ -0,0 +1,15 @@
+function constCrew(obj){
+    if(obj.dizziness===true){
+        let bonusHidr = obj.weight*obj.experience*0.1;
+        obj.levelOfHydrated+=bonusHidr;
+        obj.dizziness=false;
+    }
+
+    console.log(obj);
+}
+
+constCrew({ weight: 80,
+    experience: 1,
+    levelOfHydrated: 0,
+    dizziness: true }
+  );
