2c0c371e092a4513605863f7fdc15b22397e2210
AnitaNakeva <anita_nakevaa@abv.bg>
2022-10-16 16:48:04 +0300

    added arrayExtension

---
+++ JS Advanced/Prototypes & Inheritance/01.ArrayExtension.js
@@ -0,0 +1,41 @@
+function extendArray(){
+    let arr = [1, 2, 3];
+    
+    Array.prototype.last=function(){
+        return this[this.length-1];
+    };
+
+    Array.prototype.skip = function(n){
+        let newArr = [];
+        for(let i = n; i<this.length; i++){
+            newArr.push(this[i]);
+        }
+        return newArr;
+    }
+
+    Array.prototype.take = function(n){
+        let newArr = [];
+        for(let i = 0; i<n; i++){
+            newArr.push(this[i]);
+        }
+        return newArr;
+    }
+
+    Array.prototype.sum = function(){
+        let sum = 0;
+        for(let i = 0; i<this.length; i++){
+            sum+=this[i];
+        }
+        return sum;
+    }
+
+    Array.prototype.average = function(){
+        let sum = this.sum();
+        let count = this.length;
+        return sum/count;
+    }
+
+    console.log(arr.average());
+}
+
+extendArray();
