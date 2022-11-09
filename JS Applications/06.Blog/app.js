2cfa9ab638e817413df9963c1b27dfe07f196eaf
AnitaNakeva <anita_nakevaa@abv.bg>
2022-11-09 10:38:32 +0200

    added Blog

---
+++ JS Applications/06.Blog/app.js
@@ -0,0 +1,51 @@
+function attachEvents() {
+    document.getElementById("btnLoadPosts").addEventListener("click", getPosts);
+    document.getElementById("btnViewPost").addEventListener("click", getComments);
+}
+
+async function getComments(){
+    const postsUrl = "http://localhost:3030/jsonstore/blog/posts";
+    const commentsUrl = "http://localhost:3030/jsonstore/blog/comments";
+
+    const titleElement = document.getElementById("post-title");
+    const selectOp = document.getElementById("posts").selectedOptions[0].value;
+    const postBodyElement = document.getElementById("post-body");
+    const postUlComments = document.getElementById("post-comments");
+
+    postUlComments.textContent = "";
+    
+    const postResponse = await fetch(postsUrl);
+    const postData = await postResponse.json();
+
+    const commentsResponse = await fetch(commentsUrl);
+    const commentsData = await commentsResponse.json();
+
+    const selectedPost = Object.values(postData).find(post=>post.id==selectOp)
+    titleElement.textContent = selectedPost.title;
+    postBodyElement.textContent = selectedPost.body;
+
+    const comments = Object.values(commentsData).filter(c=>c.postId === selectOp);
+
+    comments.forEach(c=>{
+        const li = document.createElement("li");
+        li.textContent = c.text;
+        postUlComments.appendChild(li);
+    })
+}
+
+async function getPosts(){
+    const selectOp = document.getElementById("posts");
+    const url = "http://localhost:3030/jsonstore/blog/posts";
+    selectOp.innerHTML = "";
+    const response = await fetch(url);
+    const data = await response.json();
+
+     Object.values(data).forEach(post=>{
+        const op = document.createElement("option");
+        op.value = post.id;
+        op.textContent = post.title;
+        selectOp.appendChild(op);
+     })
+}
+
+attachEvents();
